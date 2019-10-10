34395717aacee609b5ed7ccdf8fd7000ed981930
ngsneto@gmail.com <silvinha1234567>
2019-10-10 18:43:47 -0300

    add

--- codigos/mysql  .R
+++ codigos/mysql  .R
@@ -6,6 +6,26 @@ rs = dbSendQuery(mydb, "select nome from marca")
 rs
 data = fetch(rs, n=-1) #executa a consulta
 data #mostra o resultado
-pie(table(data))
-
+       
+       
+       #make slices
+       slices <- c(19, 26, 55)
+       
+       # Define some colors 
+       colors <- c("yellow2","olivedrab3","orangered3")
+       
+       # Calculate the percentage for each day, rounded to one decimal place
+       slices_labels <- round(slices/sum(slices) * 100, 1)
+       
+       # Concatenate a '%' char after each value
+       slices_labels <- paste(slices_labels, "%", sep="")
+       
+       # Create a pie chart with defined heading and custom colors and labels
+       pie(slices, main="Sum", col=colors, labels=slices_labels, cex=0.8)
+       
+       
+       # Create a legend at the right   
+       legend("topright", c("DH","UT","AM"), cex=0.7, fill=colors)
+       
+?pie
 
