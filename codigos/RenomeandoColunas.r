7e3d8203d89e2108af9867169f829225ca47f66a
ngsneto@gmail.com <silvinha1234567>
2019-10-14 20:58:40 -0300

    add

--- codigos/RenomeandoColunas.r
+++ codigos/RenomeandoColunas.r
@@ -8,3 +8,9 @@ names(surveysex)[3] = "sexo"
 names(surveysex)[4] = "Estado civil"
 names(surveysex)[5] = "Bairro"
 surveysex
+
+
+Amostra <- read.table("E:\\Natanael\\tsi\\3periodo\\estatistica\\Nova\\estatistic\\EstatisticaR\\codigos\\meubd.txt", sep = "\t")
+#Uma vez salva sua amostra, voc? est? pronto para come?ar as an?lises!
+Amostra
+colnames(Amostra)[1]<-'Pais' #renomeia o nome da primeira coluna do dataframe
