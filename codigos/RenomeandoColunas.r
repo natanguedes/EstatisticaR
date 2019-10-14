5fbace2da1b7315418ba31d896389651d634437f
ngsneto@gmail.com <silvinha1234567>
2019-10-14 11:15:06 -0300

    add

---
+++ codigos/RenomeandoColunas.r
@@ -0,0 +1,10 @@
+
+
+surveysex <- c("Maria", "21", "F", "Casada", "Cristo")
+surveysex
+names(surveysex)[1] = "Nome"
+names(surveysex)[2] = "idade"
+names(surveysex)[3] = "sexo"
+names(surveysex)[4] = "Estado civil"
+names(surveysex)[5] = "Bairro"
+surveysex
