e4f4ba54ff53451ae4dce326cb6de736ca1feed3
ngsneto@gmail.com <silvinha1234567>
2019-10-15 18:39:55 -0300

    add

--- codigos/graficos/Analise.R
+++ codigos/graficos/Analise.R
@@ -1,5 +1,5 @@
 
-install.packages("csvread")
+
 
 dados =  read.table("E:\\Natanael\\tsi\\3periodo\\estatistica\\Nova\\estatistic\\EstatisticaR\\codigos\\graficos\\matriculados.txt", header = T, sep="," )
 
@@ -11,11 +11,3 @@ pie(table(un))
 freqa = table(dados['unidade_ensino']) #calcula frequencia absoluta de unidade ensino
 freqa #exibe as frequencias absolutas
 
-freqr = prop.table(dados['unidade_ensino'])
-freqr
-
-
-sum(ano)
-count.fields()
-
-colSums(un)
