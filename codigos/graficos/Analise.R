80835890c79107a2974830396d181c7ff235a0bc
ngsneto@gmail.com <silvinha1234567>
2019-10-16 08:33:28 -0300

    Gráfico de setores

--- codigos/graficos/Analise.R
+++ codigos/graficos/Analise.R
@@ -11,3 +11,9 @@ pie(table(un))
 freqa = table(dados['unidade_ensino']) #calcula frequencia absoluta de unidade ensino
 freqa #exibe as frequencias absolutas
 
+x = c(130,80,90,110,130,140,90,90,130,100,260)
+label = c("CBRA","CCEI","CEST","CGAM","CPLA","CRFI","CSAM","CSSB","CTAG","CTGC","IFB")
+porcetagem = round(100*x/sum(x),1)
+pie(table(un), label=porcetagem,main = "unidades de ensino", col = rainbow(length(x))) 
+legend(1,1, c("CBRA","CCEI","CEST","CGAM","CPLA","CRFI","CSAM","CSSB","CTAG","CTGC","IFB"), cex = 0.5,fill= rainbow(length(x)))
+
