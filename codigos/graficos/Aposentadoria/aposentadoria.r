67f797f98980b700ddbb929c7942c68c9bb24b68
ngsneto@gmail.com <silvinha1234567>
2019-10-21 20:48:07 -0300

    add

--- codigos/graficos/Aposentadoria/aposentadoria.r
+++ codigos/graficos/Aposentadoria/aposentadoria.r
@@ -5,12 +5,23 @@ pie(table("Sexo"), col = rainbow(2))
 
 table(MyData['Sexo'])
 
+#gráfico pie
+
 x <-  c(1967,2102)
 labels <-  c("Feminino","Masculino")
-
 piepercent<- round(100*x/sum(x), 1)
+lblsx <- paste(piepercent,"%",sep="") # ad % to labels
 # Plot the chart.
-pie(x, labels = piepercent, main = "Aposentadorias concedidas por anos de serviço
+pie(x, labels = lblsx, main = "Aposentadorias concedidas por anos de serviço
 ",col = rainbow(length(x)))
 legend(1.5, 0.5, c("Feminino","Masculino"), cex = 0.8,
        fill = rainbow(length(x)))
+
+
+#gráfico pie
+x <- c(32, 12, 30, 45)
+labels <- c("California", "Paris", "Moscow", "Mumbai")
+pct <- round(x/sum(x)*100)
+lblsx <- paste(pct,"%",sep="") # ad % to labels
+pie(x,labels = lblsx, col=rainbow(length(x)),main="city_pie_chart")
+legend("topright", c("California", "Paris", "Moscow", "Mumbai"), cex=0.8,fill=rainbow(length(x)))
