a4b7f8e1b056029d1371936d18f7478e5e76f2e2
natanguedes <ngsneto@gmail.com>
2019-10-23 11:48:58 -0300

    Update Codigo_R_redes_computadores.R

--- codigos/MATRICULADOS/Codigo_R_redes_computadores.R
+++ codigos/MATRICULADOS/Codigo_R_redes_computadores.R
@@ -8,6 +8,8 @@
 dados = read.table("E:\\Natanael\\tsi\\3periodo\\estatistica\\Nova\\estatistic\\EstatisticaR\\codigos\\MATRICULADOS\\matriculados.txt", header = T, sep = ",") #o separador de tabula??o ? "\t" e da v?rgula ";"
 #? necess?rio primeiro salvar o arquivo .xls (ou .xlsx) no formato .txt (serparaddo por tabula??o) ou .csv (separado por v?rgulas)
 
+#se o arquivo que voce deseja ler estiver na mesma pasta do projeto ai deve-se ler da seguinte forma:
+dados = read.table("matriculados.txt", header = T, sep = ",") #o separador de tabula??o ? "\t" e da v?rgula ";"
 
 SA = na.exclude(dados) #retira os NA'S
 Amostra <- dados[sample(nrow(SA), 60),] #GERA AMOSTRA DE 60
@@ -16,6 +18,4 @@ attach(Amostra)
 #salva a amostra
 write.table(Amostra, "E:\\Natanael\\tsi\\3periodo\\estatistica\\Nova\\estatistic\\EstatisticaR\\codigos\\MATRICULADOS\\amostra.txt", append = F, sep = "\t", row.names = F) #O comando append = F
 
-#No caso espec?fico do nosso banco, existem muitas informa??es faltantes
-#classificadas como NA pelo R. ? preciso elimin?-las com o uso da seguinte
-table(dados[,1])
+
