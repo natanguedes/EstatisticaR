c5f7d2ac58f40e263b4ce322dabb6986f46187f5
ngsneto@gmail.com <silvinha1234567>
2019-10-24 10:12:29 -0300

    add

--- codigos/graficos/Aposentadoria/aposentadoria.r
+++ codigos/graficos/Aposentadoria/aposentadoria.r
@@ -1,4 +1,4 @@
-MyData <- read.csv(file="E:\Natanael\\tsi\\3periodo\\estatistica\\Nova\\estatistic\\EstatisticaR\\codigos\\graficos\\Aposentadoria\\CON06.csv", header=TRUE, sep=",")
+MyData <- read.csv(file="E:\\Natanael\\tsi\\3periodo\\estatistica\\Nova\\estatistic\\EstatisticaR\\codigos\\graficos\\Aposentadoria\\CON06.csv", header=TRUE, sep=",")
 MyData
 
 pie(table("Sexo"), col = rainbow(2))
@@ -12,12 +12,15 @@ labels <-  c("Feminino","Masculino")
 piepercent<- round(100*x/sum(x), 1)
 lblsx <- paste(piepercent,"%",sep="") # ad % to labels
 # Plot the chart.
-pie(x, labels = lblsx, main = "Aposentadorias concedidas por anos de serviço
+pie(x, labels = lblsx, main = "Gráfico de Setores \nAposentadorias concedidas por anos de serviço
 ",col = rainbow(length(x)))
 legend(1.5, 0.5, c("Feminino","Masculino"), cex = 0.8,
        fill = rainbow(length(x)))
 
-
+barplot(table(MyData["Espécie"]),col= rainbow(5), main = "Tipo de contribuição")
+?barplot
+sa = table(MyData["Espécie"])
+sa
 #gráfico pie
 x <- c(32, 12, 30, 45)
 labels <- c("California", "Paris", "Moscow", "Mumbai")
