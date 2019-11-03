0e9e07966bb181e24aeff6da9fdef5064d84f791
ngsneto@gmail.com <silvinha1234567>
2019-11-03 07:55:24 -0300

    add

--- codigos/PythonnoR/test.py
+++ codigos/PythonnoR/test.py
@@ -8,7 +8,7 @@ x2 = ["salgado"]
 y2 = ["salgado"]
 
 # Criando um gráfico
-plt.bar(x, y, label='doce', olor='r')
+plt.bar(x, y, label='doce', color='r')
 plt.bar(x2, y2, label='salgadco', color='y')
 plt.legend(loc=9)
 plt.title("Gráficos sobre biscoitos")
