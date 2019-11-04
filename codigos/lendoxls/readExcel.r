43698225f0ff553d065e4354f825077c1e809152
ngsneto@gmail.com <silvinha1234567>
2019-11-03 22:53:12 -0300

    add

--- codigos/lendoxls/readExcel.r
+++ codigos/lendoxls/readExcel.r
@@ -6,13 +6,14 @@ install.packages("readxl")
 library("readxl")
 # xls files
 my_data <- read_excel("E:\\Natanael\\tsi\\3periodo\\estatistica\\Nova\\estatistic\\EstatisticaR\\codigos\\lendoxls\\LotOrgaoDistOcupVagas.xlsx")
-table(my_data[,1])#frequencia absoluta
+APROVADA=table(APROVADA)#frequencia absoluta
 
 a= table(my_data["NOME_ORGAO"]) #frequencia absoluta
 a
 s=prop.table(my_data["APROVADA"])#frequencia Relativa
 s
 
+aprovada = table(my_data[,5]) #aprovada é uma variável quantitativa que  está na posição 5 na variável my_data
 
-
-table(LotOrgaoDistOcupVagas[,1])
+distribuida =  table(my_data[,6]) #distribuida é uma variável quantitativa que  está na posição 6 na variável my_data
+cor(aprovada,distribuida)
