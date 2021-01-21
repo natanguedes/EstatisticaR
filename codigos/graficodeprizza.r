9e5d95cb0bcc70966f84fc679d71566bf9fa096b
ngconsultoriadedados <ngsneto@gmail.com>
2021-01-20 23:53:23 -0300

    Create graficodeprizza.r

---
+++ codigos/graficodeprizza.r
@@ -0,0 +1,7 @@
+slices <- c(10, 12,4, 16, 8)
+lbls <- c("US", "UK", "Australia", "Germany", "France")
+pie(slices, labels = lbls, main="Pie Chart of Countries")
+
+pie(x, labels = piepercent, main = "City pie chart",col = rainbow(length(x)))
+legend("topright", c("London","New York","Singapore","Mumbai"), cex = 0.8,
+       fill = rainbow(length(x)))
